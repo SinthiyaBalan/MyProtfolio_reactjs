77c0341f248429f37ef42408797b42f2268fc892
SinthiyaBalan <sinthiyamct@gmail.com>
2023-06-10 22:56:00 +0200

    Updated MainSection

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 function MainSection() {
   return (
     <main className='main-section grow'>
-        <h2>Hi, I am Sinthiya Balasubramaniam</h2>
+        <h4>Hi, My name is</h4>
+        <h2> Sinthiya Balasubramaniam</h2>
         <p>Frontend developer </p>
       </main>
   )
